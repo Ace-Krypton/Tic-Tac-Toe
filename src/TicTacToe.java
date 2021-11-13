44d711ea216eae9e130ebcb9817a6edc4f20eb85
Ace-Krypton <ramizna@code.edu.az>
2021-11-13 16:43:46 +0100

    #3

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,12 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},    //This is for, GameBoard UI
-                              {'-', '+', '-', '+', '-'},
-                              {' ', '|', ' ', '|', ' '},
-                              {'-', '+', '-', '+', '-'},
-                              {' ', '|', ' ', '|', ' '}};
+        String[][] gameBoard = {{" ", "|", " ", "|", " "},    //This is for, GameBoard UI
+                                {"-", "+", "-", "+", "-"},
+                                {" ", "|", " ", "|", " "},
+                                {"-", "+", "-", "+", "-"},
+                                {" ", "|", " ", "|", " "}};
 
         printGameBoard(gameBoard); //This is for, sending elements to the void method named "printGameBoard"
 
@@ -16,23 +17,41 @@ public class TicTacToe {
 
         System.out.println(pos);
 
-        switch(pos){
-            case 1 -> gameBoard[0][0] = 'X';
-            case 2 -> gameBoard[0][2] = 'X';
-            case 3 -> gameBoard[0][4] = 'X';
-            case 4 -> gameBoard[2][0] = 'X';
-            case 5 -> gameBoard[2][2] = 'X';
-            case 6 -> gameBoard[2][4] = 'X';
-            case 7 -> gameBoard[4][0] = 'X';
-            case 8 -> gameBoard[4][2] = 'X';
-            case 9 -> gameBoard[4][4] = 'X';
+        placePiece(gameBoard, pos, "player"); // -> User  //Our method requires gameBoard, position and user that is why we entered that values in it
+
+        Random rand = new Random();
+        int cpuPos = rand.nextInt();
+        placePiece(gameBoard, pos, "cpu");  // -> Computer //We can use AI to create moves, but in this example I used Random() method to do that
+
+        printGameBoard(gameBoard); //This is for, sending elements to the void method named "printGameBoard"
+    }
+    public static void printGameBoard(String[][] gameBoard){
+        for(String[] row : gameBoard){    //This is for, printing the elements of 2D array gameBoard
+            for(String c : row)
+                System.out.print(c + " ");
+            System.out.println();
         }
     }
-    public static void printGameBoard(char[][] gameBoard){
-        for(char[] row : gameBoard){    //This is for, printing the elements of 2D array gameBoard
-            for(char c : row)
-                System.out.print(c);
-            System.out.println();
+
+    public static void placePiece(String[][] gameBoard, int pos, String user){
+        String symbol = "X";    //Default symbol is "X" right now
+
+        if(user.equals("player"))   //If user == "player" the symbol will be "X"
+            symbol = "X";
+        else if(user.equals("cpu"))  //But if user == "cpu" it will be symbol "O"
+            symbol = "O";
+
+        switch(pos){
+            case 1 -> gameBoard[0][0] = symbol;
+            case 2 -> gameBoard[0][2] = symbol;
+            case 3 -> gameBoard[0][4] = symbol;
+            case 4 -> gameBoard[2][0] = symbol;
+            case 5 -> gameBoard[2][2] = symbol;
+            case 6 -> gameBoard[2][4] = symbol;
+            case 7 -> gameBoard[4][0] = symbol;
+            case 8 -> gameBoard[4][2] = symbol;
+            case 9 -> gameBoard[4][4] = symbol;
+            default -> System.out.println("Something went wrong, try again");
         }
     }
 }
