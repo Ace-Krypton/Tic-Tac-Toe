25ff982a2be597a4c4dc5618945a54158492ce9b
Ace-Krypton <ramizna@code.edu.az>
2021-11-13 18:22:31 +0100

    #5

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -17,14 +17,22 @@ public class TicTacToe {
             Scanner scan = new Scanner(System.in);  //Scanner() method is for player input
             System.out.print("Enter your placement (1-9): ");
             int playerPos = scan.nextInt();
+            while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)){ //This prevents 'user' to enter same value into same place
+                System.out.print("Position taken! Enter a correct position");
+                playerPos = scan.nextInt();
+            }
 
             placePiece(gameBoard, playerPos, "player"); // -> User  //Our method requires gameBoard, position and user that is why we entered that values in it
 
             Random rand = new Random();
             int cpuPos = rand.nextInt(9)+1;  //Generates random numbers between 1 and 9
+            while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)){ //This prevents 'cpu' to enter same value into same place
+                cpuPos = rand.nextInt(9)+1;
+            }
             placePiece(gameBoard, cpuPos, "cpu");  // -> Computer  //We can use AI to create moves, but in this example I used Random() method to do that
 
             printGameBoard(gameBoard); //This is for, sending elements to the void method named "printGameBoard"
+            System.out.println(checkWinner());  //This method is for checking the winner
         }
     }
     public static void printGameBoard(String[][] gameBoard){
@@ -38,10 +46,14 @@ public class TicTacToe {
     public static void placePiece(String[][] gameBoard, int pos, String user){
         String symbol = "X";    //Default symbol is "X" right now
 
-        if(user.equals("player"))   //If user == "player" the symbol will be "X"
+        if(user.equals("player")){   //If user == "player" the symbol will be "X"
             symbol = "X";
-        else if(user.equals("cpu"))  //But if user == "cpu" it will be symbol "O"
+            playerPositions.add(pos);
+        }
+        else if(user.equals("cpu")){  //But if user == "cpu" it will be symbol "O"
             symbol = "O";
+            cpuPositions.add(pos);
+        }
 
         switch(pos){
             case 1 -> gameBoard[0][0] = symbol;
@@ -68,6 +80,26 @@ public class TicTacToe {
         List cross1 = Arrays.asList(1, 5, 9);
         List cross2 = Arrays.asList(7, 5, 3);
 
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for(List l : winning){
+            if(playerPositions.containsAll(l))
+                return "Congratulations you won!";
+
+            else if(cpuPositions.containsAll(l))
+                return "Computer win :(";
+
+            else if(playerPositions.size() + cpuPositions.size() == 9)
+                return "Tie!";
+        }
 
 
         return "";
