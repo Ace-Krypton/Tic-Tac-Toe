c0fee1d79062cfbcc1f495092681860c6aa5d3df
Ace-Krypton <ramizna@code.edu.az>
2021-11-13 17:04:09 +0100

    #4

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,9 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();   //Arraylist for Player positions
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();      //Arraylist for CPU positions
+
     public static void main(String[] args) {
         String[][] gameBoard = {{" ", "|", " ", "|", " "},    //This is for, GameBoard UI
                                 {"-", "+", "-", "+", "-"},
@@ -11,19 +13,19 @@ public class TicTacToe {
 
         printGameBoard(gameBoard); //This is for, sending elements to the void method named "printGameBoard"
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your placement (1-9): ");
-        int pos = scan.nextInt();
+        while(true){    //This loop is for asking inputs again and again till program finished
+            Scanner scan = new Scanner(System.in);  //Scanner() method is for player input
+            System.out.print("Enter your placement (1-9): ");
+            int playerPos = scan.nextInt();
 
-        System.out.println(pos);
+            placePiece(gameBoard, playerPos, "player"); // -> User  //Our method requires gameBoard, position and user that is why we entered that values in it
 
-        placePiece(gameBoard, pos, "player"); // -> User  //Our method requires gameBoard, position and user that is why we entered that values in it
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9)+1;  //Generates random numbers between 1 and 9
+            placePiece(gameBoard, cpuPos, "cpu");  // -> Computer  //We can use AI to create moves, but in this example I used Random() method to do that
 
-        Random rand = new Random();
-        int cpuPos = rand.nextInt();
-        placePiece(gameBoard, pos, "cpu");  // -> Computer //We can use AI to create moves, but in this example I used Random() method to do that
-
-        printGameBoard(gameBoard); //This is for, sending elements to the void method named "printGameBoard"
+            printGameBoard(gameBoard); //This is for, sending elements to the void method named "printGameBoard"
+        }
     }
     public static void printGameBoard(String[][] gameBoard){
         for(String[] row : gameBoard){    //This is for, printing the elements of 2D array gameBoard
@@ -53,5 +55,21 @@ public class TicTacToe {
             case 9 -> gameBoard[4][4] = symbol;
             default -> System.out.println("Something went wrong, try again");
         }
+    }
+
+    public static String checkWinner(){
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+
+
+        return "";
     }
 }
